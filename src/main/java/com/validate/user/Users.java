370d585e91c291852ed48c463f8f2c3f505c9b54
kartik <kmistry1@uncc.edu>
2018-10-20 17:51:09 -0400

    valid users

---
+++ src/main/java/com/validate/user/Users.java
@@ -0,0 +1,15 @@
+package com.validate.user;
+
+import java.util.ArrayList;
+
+public class Users {
+
+    public ArrayList<String> getValidUsers() {
+        ArrayList<String> users = new ArrayList<String>();
+        users.add("nirali");
+        users.add("sunny");
+        users.add("pooja");
+        users.add("kartik");
+        return users;
+    }
+}
