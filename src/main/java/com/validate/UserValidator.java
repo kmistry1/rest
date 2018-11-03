a729ba9e23ed108f1639e5e0aea7b915cc758e07
Anuj Patel <apate139@uncc.edu>
2018-11-03 13:44:04 -0400

    password logic (#1)

--- src/main/java/com/validate/UserValidator.java
+++ src/main/java/com/validate/UserValidator.java
@@ -8,22 +8,28 @@ import java.util.ArrayList;
 
 public class UserValidator {
 
-    
-   
-    private String username;
+    private String userName;
+    private String password;
 
+    public void setUserName(String name){
+        this.userName = name;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
     public String getUserName(){
-        return username;
+        return userName;
     }
-    public void setName(String name){
-        this.username = name;
+    public String getPassword(){
+        return password;
     }
+
 
     private String checkForNullOrEmpty(String username) {
         Users validUsers = new Users();
         ArrayList<String> users = validUsers.getValidUsers();
         if(StringUtils.isBlank(username)){
-            return "username cannot be null, blank or empty";
+            return "username or password cannot be null, blank or empty";
         }
 
         for(String user:users){
@@ -33,11 +39,35 @@ public class UserValidator {
             } catch (Exception e) {
                 e.printStackTrace();
             }
+
             if(user.equals(username)){
-                return "username is valid";
+                if(checkForpassword(getPassword())){
+                    return "username and password is valid";
+                }
             }
         }
-        return "username is invalid";
+        return "username or password is invalid";
+    }
+
+    private boolean checkForpassword(String password) {
+        Users validUsers = new Users();
+        ArrayList<String> passwords = validUsers.getValidPasswors();
+        if(StringUtils.isBlank(password)){
+            return false;
+        }
+        for(String pass:passwords){
+
+            try {
+                pass = IEncryptionLoop.decrypt(pass);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            if(password.equals(password)){
+                return true;
+            }
+        }
+        return false;
     }
 
     public String getUserValidation(){
