bd1b8653768fca973de06210c32c25bc3c5a5314
kartik <kmistry1@uncc.edu>
2018-11-10 19:19:21 -0500

    encryption

--- src/main/java/com/validate/UserValidator.java
+++ src/main/java/com/validate/UserValidator.java
@@ -8,7 +8,6 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 
 public class UserValidator {
