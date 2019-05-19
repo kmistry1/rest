19de7551f5592e4539caf6a8d73b73bdd41d3147
sunny <none>
2019-05-19 17:35:54 -0400

    test

--- src/main/java/com/webapp/app/UserController.java
+++ src/main/java/com/webapp/app/UserController.java
@@ -3,9 +3,11 @@ package com.webapp.app;
 
 import com.webapp.logic.UserValidator;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import rest.UserInput;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -15,12 +17,11 @@ public class UserController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/api/resource/validation")
-    public String validator(@RequestParam(value = "username") String userName,
-                            @RequestParam(value = "password") String password) throws Exception {
+    public String validator(@RequestBody UserInput userInput) throws Exception {
 
         UserValidator userValidator = new UserValidator();
-        userValidator.setUserName(userName);
-        userValidator.setPassword(password);
+        userValidator.setUserName(userInput.getUsername());
+        userValidator.setPassword(userInput.getPassword());
 
         return userValidator.getUserValidation();
     }
